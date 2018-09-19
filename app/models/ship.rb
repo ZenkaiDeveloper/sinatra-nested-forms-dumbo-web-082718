a7a27a3363a28a6577bfebb818183c44f46298cf
Andy <wang.andy32@gmail.com>
2018-09-18 22:32:32 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_reader :name, :type, :booty
+  SHIPS=[]
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS.push(self)
+  end
+  def self.all
+    SHIPS
+  end
+  def self.clear
+    SHIPS.clear
+  end
 end
